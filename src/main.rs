71caf4d7344772824e202b3f641efa5bcfdd8a39
chevdor <chevdor@gmail.com>
2021-06-30 19:14:09 +0200

    Fix linting

--- src/main.rs
+++ src/main.rs
@@ -5,260 +5,268 @@ use std::path::PathBuf;
 use std::str;
 
 use failure::{Error, Fail};
-use serde::ser::{Serialize, SerializeMap, Serializer, SerializeSeq};
+use serde::ser::{Serialize, SerializeMap, SerializeSeq, Serializer};
 use structopt::StructOpt;
-use toml_edit::{Document, Item, Table, Value, value};
+use toml_edit::{value, Document, Item, Table, Value};
 
-use query_parser::{Query, TpathSegment, parse_query};
+use query_parser::{parse_query, Query, TpathSegment};
 
 // TODO: Get more of the description in the README into the CLI help.
 #[derive(StructOpt)]
 #[structopt(about)]
 enum Args {
-    /// Print some data from the file
-    Get {
-        /// Path to the TOML file to read
-        #[structopt(parse(from_os_str))]
-        path: PathBuf,
-        /// Query within the TOML data (e.g. `dependencies.serde`, `foo[0].bar`)
-        query: String,
-        #[structopt(flatten)]
-        opts: GetOpts,
-    },
-    /// Edit the file to set some data (currently, just print modified version)
-    Set {
-        /// Path to the TOML file to read
-        #[structopt(parse(from_os_str))]
-        path: PathBuf,
-        /// Query within the TOML data (e.g. `dependencies.serde`, `foo[0].bar`)
-        query: String,
-        /// String value to place at the given spot (bool, array, etc. are TODO)
-        value_str: String, // TODO more forms
-    },
-    // TODO: append/add (name TBD)
+	/// Print some data from the file
+	Get {
+		/// Path to the TOML file to read
+		#[structopt(parse(from_os_str))]
+		path: PathBuf,
+		/// Query within the TOML data (e.g. `dependencies.serde`, `foo[0].bar`)
+		query: String,
+		#[structopt(flatten)]
+		opts: GetOpts,
+	},
+	/// Edit the file to set some data (currently, just print modified version)
+	Set {
+		/// Path to the TOML file to read
+		#[structopt(parse(from_os_str))]
+		path: PathBuf,
+		/// Query within the TOML data (e.g. `dependencies.serde`, `foo[0].bar`)
+		query: String,
+		/// String value to place at the given spot (bool, array, etc. are TODO)
+		value_str: String, // TODO more forms
+	},
+	// TODO: append/add (name TBD)
 }
 
 #[derive(StructOpt)]
 struct GetOpts {
-    /// Print as a TOML fragment (default: print as JSON)
-    #[structopt(long)]
-    output_toml: bool,
+	/// Print as a TOML fragment (default: print as JSON)
+	#[structopt(long)]
+	output_toml: bool,
 }
 
 #[derive(Debug, Fail)]
 enum CliError {
-    #[fail(display = "bad query")]
-    BadQuery(),
-    #[fail(display = "numeric index into non-array")]
-    NotArray(),
-    #[fail(display = "array index out of bounds")]
-    ArrayIndexOob(),
+	#[fail(display = "bad query")]
+	BadQuery(),
+	#[fail(display = "numeric index into non-array")]
+	NotArray(),
+	#[fail(display = "array index out of bounds")]
+	ArrayIndexOob(),
 }
 
 fn main() -> Result<(), Error> {
-    let args = Args::from_args();
-    match args {
-        Args::Get { path, query, opts } => get(path, &query, opts)?,
-        Args::Set { path, query, value_str } => set(path, &query, &value_str)?,
-    }
-    Ok(())
+	let args = Args::from_args();
+	match args {
+		Args::Get { path, query, opts } => get(path, &query, opts)?,
+		Args::Set { path, query, value_str } => set(path, &query, &value_str)?,
+	}
+	Ok(())
 }
 
 fn read_parse(path: PathBuf) -> Result<Document, Error> {
-    // TODO: better report errors like ENOENT
-    let data = fs::read(path)?;
-    let data = str::from_utf8(&data)?;
-    Ok(data.parse::<Document>()?)
+	// TODO: better report errors like ENOENT
+	let data = fs::read(path)?;
+	let data = str::from_utf8(&data)?;
+	Ok(data.parse::<Document>()?)
 }
 
 fn get(path: PathBuf, query: &str, opts: GetOpts) -> Result<(), Error> {
-    let tpath = parse_query_cli(query)?.0;
-    let doc = read_parse(path)?;
+	let tpath = parse_query_cli(query)?.0;
+	let doc = read_parse(path)?;
 
-    if opts.output_toml {
-        print_toml_fragment(&doc, &tpath);
-    } else {
-        let item = walk_tpath(&doc.root, &tpath);
-        // TODO: support shell-friendly output like `jq -r`
-        println!("{}", serde_json::to_string(&JsonItem(item))?);
-    }
-    Ok(())
+	if opts.output_toml {
+		print_toml_fragment(&doc, &tpath);
+	} else {
+		let item = walk_tpath(&doc.root, &tpath);
+		// TODO: support shell-friendly output like `jq -r`
+		println!("{}", serde_json::to_string(&JsonItem(item))?);
+	}
+	Ok(())
 }
 
-fn print_toml_fragment(doc: &Document, tpath: &[TpathSegment]) -> () {
-    use TpathSegment::{Name, Num};
+fn print_toml_fragment(doc: &Document, tpath: &[TpathSegment]) {
+	use TpathSegment::{Name, Num};
 
-    let mut item = &doc.root;
-    let mut breadcrumbs = vec![];
-    for seg in tpath {
-        breadcrumbs.push((item, seg));
-        match seg {
-            Name(n) => item = &item[n],
-            Num(n) => item = &item[n],
-        }
-    }
+	let mut item = &doc.root;
+	let mut breadcrumbs = vec![];
+	for seg in tpath {
+		breadcrumbs.push((item, seg));
+		match seg {
+			Name(n) => item = &item[n],
+			Num(n) => item = &item[n],
+		}
+	}
 
-    let mut item = item.clone();
-    while let Some((parent, seg)) = breadcrumbs.pop() {
-        match (seg, parent) {
-            (Name(n), Item::Table(t)) => {
-                // TODO clean up all this copying; may need more from toml_edit API
-                let mut next = t.clone();
-                while !next.is_empty() {
-                    let (k, _) = next.iter().next().unwrap();
-                    let k = String::from(k);
-                    next.remove(&k);
-                }
-                next[n] = item;
-                item = Item::Table(next);
-            }
-            (Num(_), Item::ArrayOfTables(a)) => {
-                // TODO clean up this copying too
-                let mut next = a.clone();
-                next.clear();
-                match item {
-                    Item::Table(t) => { next.append(t); },
-                    _ => panic!("malformed TOML parse-tree"),
-                }
-                item = Item::ArrayOfTables(next);
-            }
-            _ => panic!("UNIMPLEMENTED: --output-toml inside inline data"), // TODO
-        }
-    }
-    let mut doc = Document::new();
-    doc.root = item;
-    print!("{}", doc.to_string());
+	let mut item = item.clone();
+	while let Some((parent, seg)) = breadcrumbs.pop() {
+		match (seg, parent) {
+			(Name(n), Item::Table(t)) => {
+				// TODO clean up all this copying; may need more from toml_edit API
+				let mut next = t.clone();
+				while !next.is_empty() {
+					let (k, _) = next.iter().next().unwrap();
+					let k = String::from(k);
+					next.remove(&k);
+				}
+				next[n] = item;
+				item = Item::Table(next);
+			}
+			(Num(_), Item::ArrayOfTables(a)) => {
+				// TODO clean up this copying too
+				let mut next = a.clone();
+				next.clear();
+				match item {
+					Item::Table(t) => {
+						next.append(t);
+					}
+					_ => panic!("malformed TOML parse-tree"),
+				}
+				item = Item::ArrayOfTables(next);
+			}
+			_ => panic!("UNIMPLEMENTED: --output-toml inside inline data"), // TODO
+		}
+	}
+	let mut doc = Document::new();
+	doc.root = item;
+	print!("{}", doc.to_string());
 }
 
 fn set(path: PathBuf, query: &str, value_str: &str) -> Result<(), Error> {
-    let tpath = parse_query_cli(query)?.0;
-    let mut doc = read_parse(path)?;
+	let tpath = parse_query_cli(query)?.0;
+	let mut doc = read_parse(path)?;
 
-    let mut item = &mut doc.root;
-    let mut already_inline = false;
-    let mut tpath = &tpath[..];
-    use TpathSegment::{Name, Num};
-    while let Some(seg) = tpath.first() {
-        tpath = &tpath[1..]; // TODO simplify to `for`, unless end up needing a tail
-        match seg {
-            Num(n) => {
-                let len = match &item {
-                    Item::ArrayOfTables(a) => a.len(),
-                    Item::Value(Value::Array(a)) => a.len(),
-                    _ => Err(CliError::NotArray())?,
-                };
-                if n >= &len {
-                    Err(CliError::ArrayIndexOob())?;
-                }
-                match &item { Item::Value(_) => already_inline = true, _ => () };
-                item = &mut item[n];
-            }
-            Name(n) => {
-                match &item {
-                    Item::Table(_) => (),
-                    Item::Value(Value::InlineTable(_)) => already_inline = true,
-                    // TODO make this more directly construct the new, inner part?
-                    _ => *item = if already_inline {
-                        Item::Value(Value::InlineTable(Default::default()))
-                    } else {
-                        Item::Table(Table::new())
-                    },
-                };
-                item = &mut item[n];
-            }
-        }
-    }
-    *item = value(value_str);
+	let mut item = &mut doc.root;
+	let mut already_inline = false;
+	let mut tpath = &tpath[..];
+	use TpathSegment::{Name, Num};
+	while let Some(seg) = tpath.first() {
+		tpath = &tpath[1..]; // TODO simplify to `for`, unless end up needing a tail
+		match seg {
+			Num(n) => {
+				let len = match &item {
+					Item::ArrayOfTables(a) => a.len(),
+					Item::Value(Value::Array(a)) => a.len(),
+					_ => return Err(CliError::NotArray().into()),
+				};
+				if n >= &len {
+					return Err(CliError::ArrayIndexOob().into());
+				}
+				if let Item::Value(_) = &item {
+					already_inline = true
+				}
+				item = &mut item[n];
+			}
+			Name(n) => {
+				match &item {
+					Item::Table(_) => (),
+					Item::Value(Value::InlineTable(_)) => already_inline = true,
+					// TODO make this more directly construct the new, inner part?
+					_ => {
+						*item = if already_inline {
+							Item::Value(Value::InlineTable(Default::default()))
+						} else {
+							Item::Table(Table::new())
+						}
+					}
+				};
+				item = &mut item[n];
+			}
+		}
+	}
+	*item = value(value_str);
 
-    // TODO actually write back
-    print!("{}", doc.to_string());
-    Ok(())
+	// TODO actually write back
+	print!("{}", doc.to_string());
+	Ok(())
 }
 
 fn parse_query_cli(query: &str) -> Result<Query, CliError> {
-    parse_query(query).map_err(|_err| {
-        CliError::BadQuery() // TODO: specific message
-    })
+	parse_query(query).map_err(|_err| {
+		CliError::BadQuery() // TODO: specific message
+	})
 }
 
-fn walk_tpath<'a>(mut item: &'a toml_edit::Item, tpath: &[TpathSegment])
-              -> &'a toml_edit::Item {
-    use TpathSegment::{Name, Num};
-    for seg in tpath {
-        match seg {
-            Name(n) => item = &item[n],
-            Num(n) => item = &item[n],
-        }
-    }
-    item
+fn walk_tpath<'a>(mut item: &'a toml_edit::Item, tpath: &[TpathSegment]) -> &'a toml_edit::Item {
+	use TpathSegment::{Name, Num};
+	for seg in tpath {
+		match seg {
+			Name(n) => item = &item[n],
+			Num(n) => item = &item[n],
+		}
+	}
+	item
 }
 
 // TODO Can we do newtypes more cleanly than this?
 struct JsonItem<'a>(&'a toml_edit::Item);
 
 impl Serialize for JsonItem<'_> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: Serializer,
-    {
-        match self.0 {
-            Item::Value(v) => JsonValue(v).serialize(serializer),
-            Item::Table(t) => JsonTable(t).serialize(serializer),
-            Item::ArrayOfTables(a) => {
-                let mut seq = serializer.serialize_seq(Some(a.len()))?;
-                for t in a.iter() {
-                    seq.serialize_element(&JsonTable(t))?;
-                }
-                seq.end()
-            }
-            Item::None => serializer.serialize_none(),
-        }
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		match self.0 {
+			Item::Value(v) => JsonValue(v).serialize(serializer),
+			Item::Table(t) => JsonTable(t).serialize(serializer),
+			Item::ArrayOfTables(a) => {
+				let mut seq = serializer.serialize_seq(Some(a.len()))?;
+				for t in a.iter() {
+					seq.serialize_element(&JsonTable(t))?;
+				}
+				seq.end()
+			}
+			Item::None => serializer.serialize_none(),
+		}
+	}
 }
 
 struct JsonTable<'a>(&'a toml_edit::Table);
 
 impl Serialize for JsonTable<'_> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: Serializer,
-    {
-        let mut map = serializer.serialize_map(Some(self.0.len()))?;
-        for (k, v) in self.0.iter() {
-            map.serialize_entry(k, &JsonItem(v))?;
-        }
-        map.end()
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		let mut map = serializer.serialize_map(Some(self.0.len()))?;
+		for (k, v) in self.0.iter() {
+			map.serialize_entry(k, &JsonItem(v))?;
+		}
+		map.end()
+	}
 }
 
 struct JsonValue<'a>(&'a toml_edit::Value);
 
 impl Serialize for JsonValue<'_> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: Serializer,
-    {
-        if let Some(v) = self.0.as_integer() {
-            v.serialize(serializer)
-        } else if let Some(v) = self.0.as_float() {
-            v.serialize(serializer)
-        } else if let Some(v) = self.0.as_bool() {
-            v.serialize(serializer)
-        } else if let Some(v) = self.0.as_str() {
-            v.serialize(serializer)
-        } else if let Some(_) = self.0.as_date_time() {
-            "UNIMPLEMENTED: DateTime".serialize(serializer) // TODO
-        } else if let Some(arr) = self.0.as_array() {
-            let mut seq = serializer.serialize_seq(Some(arr.len()))?;
-            for e in arr.iter() {
-                seq.serialize_element(&JsonValue(e))?;
-            }
-            seq.end()
-        } else if let Some(t) = self.0.as_inline_table() {
-            let mut map = serializer.serialize_map(Some(t.len()))?;
-            for (k, v) in t.iter() {
-                map.serialize_entry(k, &JsonValue(v))?;
-            }
-            map.end()
-        } else {
-            panic!("unknown variant of toml_edit::Value");
-        }
-    }
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		if let Some(v) = self.0.as_integer() {
+			v.serialize(serializer)
+		} else if let Some(v) = self.0.as_float() {
+			v.serialize(serializer)
+		} else if let Some(v) = self.0.as_bool() {
+			v.serialize(serializer)
+		} else if let Some(v) = self.0.as_str() {
+			v.serialize(serializer)
+		} else if self.0.as_date_time().is_some() {
+			"UNIMPLEMENTED: DateTime".serialize(serializer) // TODO
+		} else if let Some(arr) = self.0.as_array() {
+			let mut seq = serializer.serialize_seq(Some(arr.len()))?;
+			for e in arr.iter() {
+				seq.serialize_element(&JsonValue(e))?;
+			}
+			seq.end()
+		} else if let Some(t) = self.0.as_inline_table() {
+			let mut map = serializer.serialize_map(Some(t.len()))?;
+			for (k, v) in t.iter() {
+				map.serialize_entry(k, &JsonValue(v))?;
+			}
+			map.end()
+		} else {
+			panic!("unknown variant of toml_edit::Value");
+		}
+	}
 }
--- src/query_parser.rs
+++ src/query_parser.rs
@@ -5,110 +5,108 @@ pub struct Query(pub Vec<TpathSegment>);
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum TpathSegment {
-    Name(String),
-    Num(usize),
+	Name(String),
+	Num(usize),
 }
 
 use nom::{
-    branch::alt,
-    bytes::complete::{escaped_transform, take_while1, take_while_m_n},
-    character::complete::{char, digit1, none_of, one_of},
-    combinator::{all_consuming, map, map_res},
-    error::ErrorKind,
-    multi::many0,
-    sequence::{delimited, preceded, tuple},
-    Err, IResult,
+	branch::alt,
+	bytes::complete::{escaped_transform, take_while1, take_while_m_n},
+	character::complete::{char, digit1, none_of, one_of},
+	combinator::{all_consuming, map, map_res},
+	error::ErrorKind,
+	multi::many0,
+	sequence::{delimited, preceded, tuple},
+	Err, IResult,
 };
 
 fn hex_unicode_scalar(len: usize, s: &str) -> IResult<&str, char> {
-    map_res(
-        take_while_m_n(len, len, |c: char| c.is_ascii_hexdigit()),
-        |s: &str| char::try_from(u32::from_str_radix(s, 16).unwrap()),
-    )(s)
+	map_res(take_while_m_n(len, len, |c: char| c.is_ascii_hexdigit()), |s: &str| {
+		char::try_from(u32::from_str_radix(s, 16).unwrap())
+	})(s)
 }
 
 fn basic_string_escape(s: &str) -> IResult<&str, char> {
-    alt((
-        one_of("\\\""),
-        map(char('b'), |_| '\x08'),
-        map(char('t'), |_| '\t'),
-        map(char('n'), |_| '\n'),
-        map(char('f'), |_| '\x0c'),
-        map(char('r'), |_| '\r'),
-        preceded(char('u'), |s| hex_unicode_scalar(4, s)),
-        preceded(char('U'), |s| hex_unicode_scalar(8, s)),
-    ))(s)
+	alt((
+		one_of("\\\""),
+		map(char('b'), |_| '\x08'),
+		map(char('t'), |_| '\t'),
+		map(char('n'), |_| '\n'),
+		map(char('f'), |_| '\x0c'),
+		map(char('r'), |_| '\r'),
+		preceded(char('u'), |s| hex_unicode_scalar(4, s)),
+		preceded(char('U'), |s| hex_unicode_scalar(8, s)),
+	))(s)
 }
 
 fn basic_string(s: &str) -> IResult<&str, String> {
-    let string_body = escaped_transform(none_of("\\\""), '\\', basic_string_escape);
-    delimited(char('"'), string_body, char('"'))(s)
+	let string_body = escaped_transform(none_of("\\\""), '\\', basic_string_escape);
+	delimited(char('"'), string_body, char('"'))(s)
 }
 
 fn bare_string(s: &str) -> IResult<&str, &str> {
-    take_while1(|c: char| c.is_ascii_alphanumeric() || c == '-' || c == '_')(s)
+	take_while1(|c: char| c.is_ascii_alphanumeric() || c == '-' || c == '_')(s)
 }
 
 fn key_string(s: &str) -> IResult<&str, String> {
-    alt((basic_string, map(bare_string, String::from)))(s)
+	alt((basic_string, map(bare_string, String::from)))(s)
 }
 
 fn array_index(s: &str) -> IResult<&str, usize> {
-    map_res(digit1, |i: &str| usize::from_str_radix(i, 10))(s)
+	map_res(digit1, |i: &str| i.parse::<usize>())(s)
 }
 
 fn tpath_segment_name(s: &str) -> IResult<&str, TpathSegment> {
-    map(key_string, TpathSegment::Name)(s)
+	map(key_string, TpathSegment::Name)(s)
 }
 
 fn tpath_segment_num(s: &str) -> IResult<&str, TpathSegment> {
-    map(delimited(char('['), array_index, char(']')), TpathSegment::Num)(s)
+	map(delimited(char('['), array_index, char(']')), TpathSegment::Num)(s)
 }
 
 fn tpath_segment_rest(s: &str) -> IResult<&str, TpathSegment> {
-    alt((
-        preceded(char('.'), tpath_segment_name),
-        tpath_segment_num,
-    ))(s)
+	alt((preceded(char('.'), tpath_segment_name), tpath_segment_num))(s)
 }
 
 fn tpath(s: &str) -> IResult<&str, Vec<TpathSegment>> {
-    alt((
-        map(all_consuming(char('.')), |_| vec![]),
-        // Must start with a name, because TOML root is always a table.
-        map(tuple((tpath_segment_name, many0(tpath_segment_rest))),
-            |(hd, mut tl)| { tl.insert(0, hd); tl })
-    ))(s)
+	alt((
+		map(all_consuming(char('.')), |_| Vec::new()),
+		// Must start with a name, because TOML root is always a table.
+		map(tuple((tpath_segment_name, many0(tpath_segment_rest))), |(hd, mut tl)| {
+			tl.insert(0, hd);
+			tl
+		}),
+	))(s)
 }
 
 pub fn parse_query(s: &str) -> Result<Query, Err<(&str, ErrorKind)>> {
-    all_consuming(tpath)(s).map(|(trailing, res)| {
-        assert!(trailing.is_empty());
-        Query(res)
-    })
+	all_consuming(tpath)(s).map(|(trailing, res)| {
+		assert!(trailing.is_empty());
+		Query(res)
+	})
 }
 
 #[test]
 fn test_parse_query() {
-    use TpathSegment::{Name, Num};
-    let name = |n: &str| Name(n.to_string());
-    for (s, expected) in vec![
-        (".", Ok(vec![])),
-        ("a", Ok(vec![name("a")])),
-        ("a.b", Ok(vec![name("a"), name("b")])),
-        ("\"a.b\"", Ok(vec![name("a.b")])),
-        ("..", Err(())),
-        ("a[1]", Ok(vec![name("a"), Num(1)])),
-        ("a[b]", Err(())),
-        ("a[1].b", Ok(vec![name("a"), Num(1), name("b")])),
-        ("a.b[1]", Ok(vec![name("a"), name("b"), Num(1)])),
-    ] {
-        let actual = parse_query(s);
-        // This could use some slicker check that prints the actual on failure.
-        // Also nice would be to proceed to try the other test cases.
-        match expected {
-            Ok(q) => assert!(q == actual.unwrap().0),
-            Err(_) => assert!(actual.is_err())
-        }
-    }
+	use TpathSegment::{Name, Num};
+	let name = |n: &str| Name(n.to_string());
+	for (s, expected) in vec![
+		(".", Ok(vec![])),
+		("a", Ok(vec![name("a")])),
+		("a.b", Ok(vec![name("a"), name("b")])),
+		("\"a.b\"", Ok(vec![name("a.b")])),
+		("..", Err(())),
+		("a[1]", Ok(vec![name("a"), Num(1)])),
+		("a[b]", Err(())),
+		("a[1].b", Ok(vec![name("a"), Num(1), name("b")])),
+		("a.b[1]", Ok(vec![name("a"), name("b"), Num(1)])),
+	] {
+		let actual = parse_query(s);
+		// This could use some slicker check that prints the actual on failure.
+		// Also nice would be to proceed to try the other test cases.
+		match expected {
+			Ok(q) => assert!(q == actual.unwrap().0),
+			Err(_) => assert!(actual.is_err()),
+		}
+	}
 }
